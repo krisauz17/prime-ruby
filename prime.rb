9d96709041f7ca44dcdf958cd523140222a5e491
krishnendu basak <krisauz17@yahoo.com>
2022-05-30 17:18:11 +1000

    Prime Ruby Lab is done

--- prime.rb
+++ prime.rb
@@ -1,1 +1,9 @@
 # Add  code here!
+
+numbers = (1..1000).to_a
+def prime(numbers)
+    numbers.select{|prime| prime.even?}
+end
+puts prime(numbers)
+
+puts "list of prime numbers from 1 to 100 are: #{(1..100).to_a.select{|n| n.even?}}" 
